3b9cbaf7903bcd490ddd24880ef9b8b13451ff51
Karl Blankenship <Karleb777@gmail.com>
2018-11-11 13:27:24 -0600

    Creating my own binary tree class.

--- src/cosc2436binarytree/BinaryTree.java
+++ src/cosc2436binarytree/BinaryTree.java
@@ -12,35 +12,7 @@ package cosc2436binarytree;
 public class BinaryTree 
 {
     
-    private class Node
-    {
-        int value;      // Integer value to be held in the node.
-        Node left;      // left pointer.
-        Node right;     // Right pointer.
-        
-        private Node(int val)
-        {
-            this.value = val;
-        }
-    }
     
-    Node root = null;
-    
-    public Node add(Node current, int val)
-    {
-        if (current == null)
-            return new Node(val);
-        else if (val < current.value)
-            current.left = add(current.left, val);
-        else if (val > current.value)
-            current.right = add(current.right, val);
-        return current;    
-    }
-    
-    public void addValue()
-    {
-        root = add(root, 10);
-    }
 
     /**
      * @param args the command line arguments
